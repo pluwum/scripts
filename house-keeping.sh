12b0b728c6e5b1b11ddb263b92a3d83937a23b6e
Luwum Patrick Alvin <luwyxx@gmail.com>
2018-01-11 19:34:30 +0300

    [Feature #1] Create file

---
+++ house-keeping.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# A  shell script that creates directories, 
+# moves to a different directories, 
+# copies files, lists directory contents, 
+# executes commands with parameters, pipes and redirects input
