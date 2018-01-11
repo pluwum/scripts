8e5a84245052873be70d4cba1f9fc42739a81615
Luwum Patrick Alvin <luwyxx@gmail.com>
2018-01-11 20:15:04 +0300

    [Feature #1] Add basic command to create, change directories and copy files

--- house-keeping.sh
+++ house-keeping.sh
@@ -6,17 +6,22 @@
 
 if  [[ $1 = "-c" ]]; then
     echo "Option -c for creating a new directory"
+    mkdir $2
+    echo "Directory with name $2 Created"
 
 elif [[ $1 = "-m" ]]; then
     echo "Option -m for moving to a different directory"
+    cd $2
+    echo "Directory changed to $2"
 
 elif [[ $1 = "-l" ]]; then
     echo "Option -l lists directory contents"
+    ls -alh
 
 elif [[ $1 = "-f" ]]; then
     echo "Option -f copies files"
-
+    cp -a $2 $3
 else
     echo "No Option supplied to script"
-    
+
 fi
