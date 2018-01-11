24758d177fa119a5a087d755f1181448d290263e
Luwum Patrick Alvin <luwyxx@gmail.com>
2018-01-11 19:56:17 +0300

    [Feature #1] Layout script command structure.

--- house-keeping.sh
+++ house-keeping.sh
@@ -3,3 +3,20 @@
 # moves to a different directories, 
 # copies files, lists directory contents, 
 # executes commands with parameters, pipes and redirects input
+
+if  [[ $1 = "-c" ]]; then
+    echo "Option -c for creating a new directory"
+
+elif [[ $1 = "-m" ]]; then
+    echo "Option -m for moving to a different directory"
+
+elif [[ $1 = "-l" ]]; then
+    echo "Option -l lists directory contents"
+
+elif [[ $1 = "-f" ]]; then
+    echo "Option -f copies files"
+
+else
+    echo "No Option supplied to script"
+    
+fi
