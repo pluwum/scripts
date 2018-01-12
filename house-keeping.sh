932720e1a2f7ebc2a742bd4a1370cb0308f89762
Luwum Patrick Alvin <luwyxx@gmail.com>
2018-01-12 10:31:29 +0300

    [Feature #1] Add help text to the script

--- house-keeping.sh
+++ house-keeping.sh
@@ -22,6 +22,11 @@ elif [[ $1 = "-f" ]]; then
     echo "Option -f copies files"
     cp -a $2 $3
 else
-    echo "No Option supplied to script"
+    echo "No Option supplied to script "
+    echo "USAGE: 
+    -c [filename] - Creates a driectory with the specified file name
+    -m [path]     - Moves the user to the specified path
+    -l [path]     - Lists the contents of the path specified
+    -f [source] [destination] - Copy source file to the destination specified."
 
 fi
